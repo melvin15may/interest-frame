07e45ae593a735092ee2b5f2e88fd0c48c3584d0
Melvin Mathew <melvin15may@gmail.com>
2017-11-07 20:37:03 -0800

    raise error on call

--- scenes.py
+++ scenes.py
@@ -1,7 +1,13 @@
-from subprocess import call
+from subprocess import check_output
+
 
 # Save scenes from video
 def save_scenes(input_path, output_path='output.mkv', detector="content", threshold=30):
-    # Add Python endpoints when more stable version for pyscenedetect is available
-    call(["scenedetect", "-i", input_path, "-d", detector, "-t", str(threshold), "-o", output_path])
+    # Add Python endpoints when more stable version for pyscenedetect is
+    # available
+    check_output(["scenedetect", "-i", input_path, "-d",
+                  detector, "-t", str(threshold), "-o", output_path])
     return
+
+
+save_scenes('asd')
